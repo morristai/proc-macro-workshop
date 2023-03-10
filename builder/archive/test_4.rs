447bf81f7c7c0de3e7caf3d1736bd4984ce36112
Morris Tai <morristai01@gmail.com>
2023-03-10 00:36:42 -0500

    chore: add some notes inside builder test 234

--- builder/archive/test_4.rs
+++ builder/archive/test_4.rs
@@ -1,5 +1,4 @@
 use proc_macro::TokenStream;
-
 use quote::quote;
 use syn::spanned::Spanned;
 
@@ -49,7 +48,6 @@ fn generate_setter_functions(fields: &StructFields) -> syn::Result<proc_macro2::
     let idents: Vec<_> = fields.iter().map(|f| { &f.ident }).collect();
     let types: Vec<_> = fields.iter().map(|f| { &f.ty }).collect();
 
-    // 创建一个空的TokenStream
     let mut final_tokenstream = proc_macro2::TokenStream::new();
 
     for (ident, type_) in idents.iter().zip(types.iter()) {
@@ -59,7 +57,6 @@ fn generate_setter_functions(fields: &StructFields) -> syn::Result<proc_macro2::
                 self
             }
         };
-        // 不断追加新的TokenStream片段到一个公共的TokenStream上
         final_tokenstream.extend(tokenstream_piece);
     }
 
@@ -92,7 +89,9 @@ fn generate_build_function(fields: &StructFields, origin_struct_ident: &syn::Ide
     let token_stream = quote! {
         pub fn build(&mut self) -> std::result::Result<#origin_struct_ident, std::boxed::Box<dyn std::error::Error>> {
             #(#checker_code_pieces)*
-                               //  ^--注意，由于我们要重复的是一组if判断代码块，它们之间不需要用逗号分隔，所以这里的重复模式是`*`，而不是之前重复结构体字段时用到的`,*`
+                               //  ^--Note that because we want to repeat a set of IF judgment code blocks,
+                               //     they do not need to be separated by comma, so the repeated mode here is `*`, not `,*` that the previous duplicate structure field is used.
+            // build a Command struct
             let ret = #origin_struct_ident{
                 #(#fill_result_clauses),*
             };
@@ -113,7 +112,7 @@ fn do_expand(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
     let builder_struct_fields_def = generate_builder_struct_fields_def(fields)?;
     let builder_struct_factory_init_clauses = generate_builder_struct_factory_init_clauses(fields)?;
     let setter_functions = generate_setter_functions(fields)?;
-    // 下面这一行是第四关新加的
+    // for test 4
     let generated_builder_functions = generate_build_function(fields, struct_ident)?;
 
     let ret = quote! {
@@ -129,7 +128,7 @@ fn do_expand(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
         }
         impl #builder_name_ident {
             #setter_functions
-            // 下面这1行是第四关新加的
+            // for test 4
             #generated_builder_functions
         }
     };
--- main.rs
+++ main.rs
@@ -11,8 +11,8 @@ use derive_builder::Builder;
 #[derive(Builder)]
 pub struct Command {
     executable: String,
-    arg: Vec<String>,
     args: Vec<String>,
+    env: Vec<String>,
     current_dir: Option<String>,
 }
 
